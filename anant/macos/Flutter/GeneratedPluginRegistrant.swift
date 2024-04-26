3b7ae9b6aad2e4958637449383e40ec8c1db2460
Coder1010ayush <coderayush1010@gmail.com>
2024-04-26 18:56:45 +0530

    "fixes"

--- anant/macos/Flutter/GeneratedPluginRegistrant.swift
+++ anant/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_blue
-import flutter_blue_plus
 import package_info
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterBluePlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlugin"))
-  FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
